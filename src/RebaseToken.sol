1fbc3d6ef82fbad49c7809b4e32fa15a8c365544
Miguel Mota <mota.m.miguel@gmail.com>
2025-07-28 20:24:53 +0100

    Add function to get balance of user with principal and accrued interest

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -40,6 +40,10 @@ contract RebaseToken is ERC20, Ownable, AccessControl {
     /*//////////////////////////////////////////////////////////////
                             STATE VARIABLES
     //////////////////////////////////////////////////////////////*/
+    uint256 private constant PRECISION_FACTOR = 1e18;
+    uint256 public s_interestRate = 5e10;  // 5e-6 percent interest per second with e18 precision
+    mapping(address user => uint256 interestRate) private s_interestsRatesByUser;
+    mapping(address user => uint256 updatedAt) private s_lastUpdatesByUser;
 
     /*//////////////////////////////////////////////////////////////
                                  EVENTS
@@ -78,7 +82,27 @@ contract RebaseToken is ERC20, Ownable, AccessControl {
     //////////////////////////////////////////////////////////////*/
 
     /*//////////////////////////////////////////////////////////////
-                           PRIVATE FUNCTIONS
+                        VIEW & PURE FUNCTIONS
     //////////////////////////////////////////////////////////////*/
-    function balanceOf(address _user) public view override returns(uint256) {}
+    function _calculateUserInterestSinceLastUpdate(address _user, uint256 principal) private view returns (uint256) {
+        // It should never happen that a user has principal but has never had an update.
+        // But let's assert that we have a last updated time just in case.
+        if (s_lastUpdatesByUser[_user] == 0) {
+            return 0;
+        }
+
+        return principal * (s_interestsRatesByUser[_user] * (block.timestamp - s_lastUpdatesByUser[_user])) / PRECISION_FACTOR;
+    }
+
+    /**
+     * 
+     * @param _user The user whose balance we want to check
+     * @notice This returns the sum of the user's principal (tokens minted) and their accrued interest since the user last interacted with the protocol
+     */
+    function balanceOf(address _user) public view override returns(uint256) {
+        uint256 principal = super.balanceOf(_user);
+        uint256 interest = _calculateUserInterestSinceLastUpdate(_user, principal);
+
+        return principal + interest;
+    }
 }
